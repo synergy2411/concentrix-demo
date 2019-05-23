967340c37d49ec405e23f51dc6440e38e4739e8e
synergy2411 <synergy2411@outlook.com>
2019-05-23 10:01:44 +0530

    users component added

---
+++ users-app/src/app/users/users.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector : "app-users",
+    template : `<h1>Users component loaded successfully!</h1>`
+})
+export class UsersComponent{}
